95081497863828d6b68eb0c6777392474ab2bb9a
Ahmed Amdagas <40519703+ahmedamdagas@users.noreply.github.com>
2026-04-19 12:26:15 +0100

    Add files via upload

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -196,7 +196,7 @@ export default function Events() {
 
     const getMilestoneData = async () => {
       try {
-        const { data: response } = await supabase.functions.invoke('generate-smart-feed', {
+        const { data: response } = await supabase.rpc('generate-smart-feed', {
           body: { 
             user_id: user.id, 
             user_lat: location.latitude, 
